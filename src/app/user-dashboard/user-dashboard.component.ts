ded6a14608383f1215dbd18c6d62771f0f8db0ac
LoranBaker <loran7081@gmail.com>
2021-09-21 16:07:08 +0200

    Added user dashboard with add, edit, delete method connected on json server

---
+++ src/app/user-dashboard/user-dashboard.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder,FormGroup} from '@angular/forms'
+import { UserModel } from './user.model';
+import { ApiService } from '../shared/api.service';
+
+
+@Component({
+  selector: 'app-user-dashboard',
+  templateUrl: './user-dashboard.component.html',
+  styleUrls: ['./user-dashboard.component.css']
+})
+export class UserDashboardComponent implements OnInit {
+
+  formValue !: FormGroup;
+  userModelObject : UserModel = new UserModel();
+  userData !: any;
+  constructor(private formBuilder : FormBuilder, 
+    private api : ApiService) { }
+
+
+
+
+  ngOnInit(): void {
+    this.formValue = this.formBuilder.group({
+      firstName: [''],
+      lastName: [''],
+      email: [''],
+      password: ['']
+    })
+    this.getAllUsers();
+  }
+
+  postUserData(){
+    this.userModelObject.firstName = this.formValue.value.firstName;
+    this.userModelObject.lastName = this.formValue.value.lastName;
+    this.userModelObject.email = this.formValue.value.email;
+    this.userModelObject.password = this.formValue.value.password;
+
+    this.api.postUser(this.userModelObject)
+    .subscribe(res=>{
+      console.log(res);
+      alert("User is added Successufully")
+      let ref = document.getElementById('close')
+      ref?.click();
+      this.formValue.reset();
+      this.getAllUsers();
+    },
+    err=>{
+      alert("Something went wrong");
+    })
+  }
+
+  getAllUsers(){
+    this.api.getUser()
+    .subscribe(res=>{
+        this.userData = res;
+    })
+  }
+
+  deleteUser(row : any){
+    this.api.deleteUser(row.id)
+    .subscribe(res=>{
+      alert("User is deleted!")
+      this.getAllUsers();
+    })
+  }
+
+  editUser(row: any){
+    this.formValue.controls['firstName'].setValue(row.firstName)
+    this.formValue.controls['lastName'].setValue(row.lastName)
+    this.formValue.controls['email'].setValue(row.email)
+    this.formValue.controls['password'].setValue(row.password)
+  }
+
+  updateUser(){
+    this.userModelObject.firstName = this.formValue.value.firstName;
+    this.userModelObject.lastName = this.formValue.value.lastName;
+    this.userModelObject.email = this.formValue.value.email;
+    this.userModelObject.password = this.formValue.value.password;
+    this.api.updateUser(this.userModelObject,this.userModelObject.id)
+  }
+}
